8d79c01af0fd3188acaa9a195c778815a43c29b5
Tô Anh Tuấn <toanhtuan.18dz@gmail.com>
2025-07-27 23:35:18 +0700

    first commit

---
+++ Ôn tập/Final Exam _001/Bai2.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+Node*createNode(int value) {
+    Node*newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+//them gtri vaoBST
+Node* insert(Node*root, int value) {
+    if (root== NULL) {
+        printf("da them gia tri %d vao cay\n", value);
+        return createNode(value);
+    }
+    if (value<root->data) {
+        root->left=insert(root->left, value);
+    }else if (value>root->data) {
+        root->right=insert(root->right, value);
+    }else {
+        printf("gia tri da ton tai\n");
+    }
+    return root;
+}
+
+//tim node min de xoa
+Node*findMin(Node*root) {
+    while (root&&root->left!=NULL) {
+        root=root->left;
+    }
+    return root;
+}
+
+Node*deleteNode(Node*root, int value) {
+    if (root==NULL) {
+        printf("khong tim thay gia tri de xoa\n");
+        return NULL;
+    }
+    if (value<root->data) {
+        root->left=deleteNode(root->left, value);
+    }else if (value>root->data) {
+        root->right=deleteNode(root->right, value);
+    }else {
+        // node co 1 hoac khog co con
+        if (root->left==NULL) {
+            Node*temp=root->right;
+            free(root);
+            printf("da xoa gia tri %d khoi cay\n", value);
+            return temp;
+        }else if (root->right==NULL) {
+            Node*temp=root->left;
+            free(root);
+            printf("da xoa gia tri %d khoi cay\n", value);
+            return temp;
+        }
+        //node co 2 con
+        Node*temp = findMin(root->right);
+        root->data=temp->data;
+        root->right=deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+//duet cay theo inorder (L-N-R)
+void inorder(Node*root) {
+    if (root==NULL) {
+        return;
+    }
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+//dem so la
+int countLeaves(Node*root) {
+    if (root==NULL) {
+        return 0;
+    }
+    if (root->left==NULL && root->right==NULL) {
+        return 1;
+    }
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+//tinh chieu cao cua cay
+int height(Node*root) {
+    if (root==NULL) {
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    int maxHeight;
+    if (leftHeight > rightHeight) {
+        maxHeight = leftHeight;
+    }else {
+        maxHeight = rightHeight;
+    }
+    return 1+maxHeight;
+}
+
+int main() {
+    Node*root = NULL;
+    int choice, x;
+    do {
+        printf("\n=== BINARY SEARCH TREE MANAGER ===\n");
+        printf("1. THEM GIA TRI x\n");
+        printf("2. XOA GIA TRI X\n");
+        printf("3. DUYET GIA TRI (INORDER)\n");
+        printf("4. DEM LA\n");
+        printf("5. CHIEU CAO\n");
+        printf("6. THOAT\n");
+        printf("Lua chon cua ban: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("nhap gia tri can them: ");
+                scanf("%d", &x);
+                root=insert(root, x);
+                break;
+            case 2:
+                printf("nhap gia tri can xoa: ");
+                scanf("%d", &x);
+                root=deleteNode(root, x);
+                break;
+            case 3:
+                if (root==NULL) {
+                    printf("cay rong\n");
+                }else {
+                    printf("duyet Inorder: ");
+                    inorder(root);
+                    printf("\n");
+                }
+                break;
+            case 4:
+                printf("so luong nut la: %d\n", countLeaves(root));
+                break;
+            case 5:
+                printf("chieu cao cua cay: %d\n", height(root));
+                break;
+            case 6:
+                printf("thoat chuong trinh\n");
+                break;
+            default:
+                printf("lua chon khong hop le. thu lai\n");
+        }
+
+    }while (choice != 6);
+    return 0;
+}
